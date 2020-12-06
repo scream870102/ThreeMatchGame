38c83c76620e2448c611929b6b6cd985999747ed
Scream <scream870102@gmail.com>
2020-12-07 02:37:16 +0900

    Pointer Interface Test

---
+++ TmUnity/Assets/Scripts/ANode.cs
@@ -0,0 +1,33 @@
+﻿//TODO: CHECK DATA USE https://docs.unity3d.com/2018.3/Documentation/ScriptReference/EventSystems.PointerEventData.html
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace TmUnity
+{
+    class ANode : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+    {
+        // Start is called before the first frame update
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+        public void OnPointerUp(PointerEventData e) {
+            Debug.Log($"Pointer UP Position {e.position} Raycast Result {e.pointerCurrentRaycast.gameObject.name} Gameobject {e.pointerPress.name}");
+        }
+        public void OnPointerDown(PointerEventData e) {
+            Debug.Log($"Pointer DOWN Position {e.position} ");
+            Debug.Log($"Raycast Result {e.pointerCurrentRaycast.gameObject.name}");
+            Debug.Log($"Gameobject {e.rawPointerPress.name}");
+
+        }
+
+    }
+
+}
+
