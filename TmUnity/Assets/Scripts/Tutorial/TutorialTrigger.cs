fe8c3fd176c0e6d2b890c9b86b2409bfbea3148a
Scream <scream870102@gmail.com>
2021-05-01 21:24:42 +0800

    Add:Tutorial

---
+++ TmUnity/Assets/Scripts/Tutorial/TutorialTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TmUnity
+{
+    class TutorialTrigger : MonoBehaviour
+    {
+        public void ToggleTutorial()
+        {
+            var currentState = gameObject.activeSelf;
+            gameObject.SetActive(!currentState);
+        }
+    }
+
+}
